1060b3a2113843ee3a65673a714ebedcb0e975df
songxiaolong <sxl13694658890@163.com>
2022-02-14 10:03:14 +0800

    react 项目初始

---
+++ src/pages/login.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+export default class login extends Component {
+  onFinish = (values) => {
+        console.log('Success:', values);
+      };
+    
+    onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+      };
+  render() {
+    return (
+        <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={this.onFinish}
+        onFinishFailed={this.onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+  
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+  
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+  
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    )
+  }
+}
